a6722f6bc22e0c4327a395cc64713f09a3e24372
sofia-papa <papa.sofia@outlook.it>
2021-08-04 16:23:10 +0100

    start password generator

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+
+var nome = prompt("Inserisci il tuo nome");
+var cognome = prompt ("Inserisci il tuo cognome");
+var colorePreferito = prompt ("Inserisci il tuo colore preferito")
+
+document.getElementById("nome").innerHTML = nome;
+document.getElementById("cognome").innerHTML = cognome;
+document.getElementById("colore-preferito").innerHTML = colorePreferito;
+
